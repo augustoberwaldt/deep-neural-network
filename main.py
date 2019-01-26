07ec672885a6ed5658bad88a3160d39c403efc50
augustoberwaldt <augusto.berwaldt@gmail.com>
2019-01-26 03:05:04 -0200

    FIX:Ajusta estrutura

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import sys
 
-from  src.app import Application
+from src.app import Application
+from pyspark.ml.classification import LogisticRegression
+
+from pyspark import SparkConf, SparkContext
+from pyspark.ml.regression import LinearRegression
 
 
 def main():
-    
     r = Application()
-    r.run() 
-
-
+    r.run()
 
 
 if __name__ == '__main__':
-    main()    
+    main()
--- src/app.py
+++ src/app.py
@@ -1,8 +1,26 @@
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 
 class Application:
-    
-             
-    def run(self):
-        print("-------------- Inciando ------------------------") 
 
+    def loadConfig(self):
+        with open('data/config.json') as data_file:
+            data = json.load(data_file)
+
+        return data
+
+    def run(self):
+        
+        
+        print("@--------------------| Inciando Leitura dataset |--------------------------@")
+
+        print("@--------------------| Inciando Keras |--------------------------@")
+        print(tf.__version__)
+        print("@--------------------| Inciando Deep Deural Networking |------------------------@")
